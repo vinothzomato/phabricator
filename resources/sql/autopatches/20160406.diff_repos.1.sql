6c676a2c0688dd693b4688c7765d5511846bb1fd
Vinoth Kumar <vinothsparrow@live.com>
2016-04-12 12:23:19 +0530

    reviewerPHID key removed

--- resources/sql/autopatches/20160406.diff_repos.1.sql
+++ resources/sql/autopatches/20160406.diff_repos.1.sql
@@ -1,8 +1,8 @@
-ALTER TABLE {$NAMESPACE}_differential.differential_diff
-  ADD repo VARCHAR(255) COLLATE utf8_general_ci DEFAULT NULL;
+#ALTER TABLE {$NAMESPACE}_differential.differential_diff
+#  ADD repo VARCHAR(255) COLLATE utf8_general_ci DEFAULT NULL;
 
-ALTER TABLE {$NAMESPACE}_differential.differential_diff
-  ADD base VARCHAR(255) COLLATE utf8_general_ci DEFAULT NULL;
+#ALTER TABLE {$NAMESPACE}_differential.differential_diff
+#  ADD base VARCHAR(255) COLLATE utf8_general_ci DEFAULT NULL;
 
-ALTER TABLE {$NAMESPACE}_differential.differential_diff
-  ADD head VARCHAR(255) COLLATE utf8_general_ci DEFAULT NULL;    
+#ALTER TABLE {$NAMESPACE}_differential.differential_diff
+#  ADD head VARCHAR(255) COLLATE utf8_general_ci DEFAULT NULL;    
