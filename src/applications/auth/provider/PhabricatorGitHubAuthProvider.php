314cfd2245efdaedc200e21205658b460d67c4ea
Vinoth Kumar <vinothsparrow@live.com>
2016-04-12 16:54:34 +0530

    scope updated

--- src/applications/auth/provider/PhabricatorGitHubAuthProvider.php
+++ src/applications/auth/provider/PhabricatorGitHubAuthProvider.php
@@ -11,7 +11,7 @@ final class PhabricatorGitHubAuthProvider
     $adapter = $this->getAdapter();
     $adapter->setState($this->getAuthCSRFCode($request));
 
-    $adapter->setScope('repo,read:org');
+    $adapter->setScope('user,repo,read:org');
 
     $attributes = array(
       'method' => 'GET',
